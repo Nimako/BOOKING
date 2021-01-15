d86c869caf2ce0d3372c87b64e63b14337df6a9e
gyasinimako.gh <gyasinimako.gh@gmail.com>
2021-01-15 03:11:30 +0000

    a

---
+++ database/migrations/2021_01_15_030330_create_admin_user.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_user', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            // Fields
+            $table->uuid('uuid');
+            $table->string('fullname')->nullable();
+            $table->string('phone_no')->nullable();
+            $table->string('email')->unique();
+            $table->text('password')->nullable();
+            $table->string('status')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_user');
+    }
+}
