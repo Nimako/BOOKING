19aa7d83ae0eb72804f822a734e91a4e1b107a36
Osborne <osborne.mordred@gmail.com>
2021-02-16 23:41:14 +0000

    pushed

--- app/Http/Controllers/NewPropertyListingController.php
+++ app/Http/Controllers/NewPropertyListingController.php
@@ -105,7 +105,7 @@ class NewPropertyListingController extends Controller
             {
                foreach ($request->details as $detailss) {
                   # variable declaration
-                  $img = $uuid = $room_name = $total_guest_capacity = $total_bathrooms = $num_of_rooms = $price_list = array();
+                  $img = $uuid = $room_name = $total_guest_capacity = $total_bathrooms = $num_of_rooms = $price_list = $similiar_rooms = array();
                   if(!empty(@$detailss['image_paths']))
                      $images = implode(STRING_GLUE, @$detailss['image_paths']);
 
