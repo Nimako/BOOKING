f5cfe1419161ee3e319068bea072102df7ea0718
Osborne Mordreds <osborne.mordred@gmail.com>
2021-02-20 19:25:05 +0000

    pushes

--- app/Services/ApartmentService.php
+++ app/Services/ApartmentService.php
@@ -166,8 +166,10 @@ class ApartmentService
             break;
 
          case 'Stage11':
-            $searchedApartment = ApartmentDetail::where(['uuid' => $request->apartment_id])->first();
-            $searchedApartment->update(['price_list' => json_encode($request->price_list)]);
+            if($searchedApartment = ApartmentDetail::where(['uuid' => $request->apartment_id])->first())
+               $searchedApartment->update(['price_list' => json_encode($request->price_list)]);
+            else 
+               return ApiResponse::returnErrorMessage($msg = "Invalid Apartment ID");
             break;
 
          case 'Stage12':
